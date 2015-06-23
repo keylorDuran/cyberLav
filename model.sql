680b03d0a4b443dfa59db2e5aafe65732db5cb78
kodurana <kodurana@est.utn.ac.cr>
2015-06-23 11:36:10 -0600

    Primer Subida, Laravel 5.1 LTS Starter

---
+++ model.sql
@@ -0,0 +1,28 @@
+CREATE TABLE song(
+	id serial not null ,
+	name VARCHAR(60) ,
+	artist_id int not null ,
+	url_source VARCHAR(300) ,
+	PRIMARY KEY (id) 
+);
+
+CREATE TABLE artist(
+	id serial not null ,
+	name VARCHAR(60) ,
+	PRIMARY KEY (id) 
+);
+
+CREATE TABLE "user" (
+	id serial NOT NULL,
+	email VARCHAR(60) NOT NULL ,
+	password VARCHAR(60) NOT NULL,	
+	is_admin BOOLEAN NOT NULL DEFAULT FALSE ,
+	PRIMARY KEY (id) ,
+	UNIQUE(email)
+);
+
+
+
+
+
+ALTER TABLE song ADD constraint fk_song_artist FOREIGN KEY (artist_id) REFERENCES artist (id);
